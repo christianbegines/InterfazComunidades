159fc54903a3871224222c166ad40dfc25b12520
christianbegines <christianbeginescarrasco@gmail.com>
2016-05-03 10:08:33 +0200

    funcion delete y funcion editar mejorada

--- src/ejerecicio2/AppComunidades.java
+++ src/ejerecicio2/AppComunidades.java
@@ -8,6 +8,7 @@ package ejerecicio2;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,8 +20,7 @@ import static sun.security.jgss.GSSUtil.login;
  * @author daw1
  */
 public class AppComunidades extends javax.swing.JFrame {
-    
-    
+
     /**
      * Creates new form AppComunidades
      */
@@ -28,6 +28,7 @@ public class AppComunidades extends javax.swing.JFrame {
         initComponents();
         establecerConexion();
     }
+
     public void establecerConexion() {
         try {
             conn = DriverManager.getConnection(url, login, password);
@@ -41,7 +42,21 @@ public class AppComunidades extends javax.swing.JFrame {
             System.out.println(e);
         }
     }
-    
+
+    public int buscarId() {
+        PreparedStatement psSelectNombre;
+        int id = 0;
+        try {
+            psSelectNombre = conn.prepareStatement(this.selectNombre);
+            psSelectNombre.setString(1, (String) this.listaComunidades.getSelectedItem());
+            rs = psSelectNombre.executeQuery();
+            rs.next();
+            id = rs.getInt("id");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "!Error " + ex.getMessage() + ex.getErrorCode(), "Ventana check", JOptionPane.INFORMATION_MESSAGE);
+        }
+        return id;
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -63,9 +78,9 @@ public class AppComunidades extends javax.swing.JFrame {
         bNuevo = new javax.swing.JButton();
         lConexion = new javax.swing.JLabel();
         lCodigo = new javax.swing.JLabel();
-        textCodigo = new javax.swing.JTextField();
         bEditar = new javax.swing.JButton();
         bBorrar = new javax.swing.JButton();
+        listaComunidades = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -74,7 +89,7 @@ public class AppComunidades extends javax.swing.JFrame {
         textId.setBackground(new java.awt.Color(204, 255, 204));
         textId.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
 
-        lNombre.setText("Nombre comunidad");
+        lNombre.setText("Nombre parque");
 
         textNombre.setBackground(new java.awt.Color(204, 255, 204));
         textNombre.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
@@ -82,6 +97,7 @@ public class AppComunidades extends javax.swing.JFrame {
         lNombre1.setText("Extension");
 
         textExtension.setBackground(new java.awt.Color(204, 255, 204));
+        textExtension.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         textExtension.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
 
         jLabel1.setText("Km2");
@@ -96,9 +112,6 @@ public class AppComunidades extends javax.swing.JFrame {
         lCodigo.setText("Codigo Comunidad");
         lCodigo.setToolTipText("");
 
-        textCodigo.setBackground(new java.awt.Color(204, 255, 204));
-        textCodigo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-
         bEditar.setText("EDIT");
         bEditar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -110,6 +123,13 @@ public class AppComunidades extends javax.swing.JFrame {
         bBorrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 bBorrarActionPerformed(evt);
+            }
+        });
+
+        listaComunidades.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Andalucia", "Aragon", "Castilla La Mancha", "Canarias", "Comunidad Valenciana" }));
+        listaComunidades.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                listaComunidadesItemStateChanged(evt);
             }
         });
 
@@ -127,12 +147,11 @@ public class AppComunidades extends javax.swing.JFrame {
                                     .addComponent(lCodigo)
                                     .addComponent(lNombre1))
                                 .addGap(48, 48, 48)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(textExtension, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(18, 18, 18)
-                                        .addComponent(jLabel1))
-                                    .addComponent(textCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(textExtension, javax.swing.GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
+                                    .addComponent(listaComunidades, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel1))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(lNombre)
@@ -173,11 +192,15 @@ public class AppComunidades extends javax.swing.JFrame {
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(textExtension, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(textCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(lCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(listaComunidades, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -193,44 +216,113 @@ public class AppComunidades extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void bNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bNuevoActionPerformed
+        PreparedStatement psInsert;
+        PreparedStatement psSelectIdParque;
         try {
-            ps=conn.prepareStatement(this.insertParque);
-            ps.setString(1,this.textNombre.getText());
-            ps.setDouble(2,Double.parseDouble(this.textExtension.getText()));
-            ps.setInt(3,Integer.parseInt(this.textCodigo.getText()));  
-            int hecho=ps.executeUpdate();
-            if(hecho!=0){
-                JOptionPane.showMessageDialog(null,"Insercion correcta","Ventana check",JOptionPane.INFORMATION_MESSAGE);
-            }else{
-            JOptionPane.showMessageDialog(null,"Insercion incorrecta","Ventana check",JOptionPane.WARNING_MESSAGE);
+            psInsert = conn.prepareStatement(this.insertParque);
+            psInsert.setString(1, this.textNombre.getText());
+            psInsert.setDouble(2, Double.parseDouble(this.textExtension.getText()));
+            psInsert.setInt(3, buscarId());
+            int hecho = psInsert.executeUpdate();
+
+            if (hecho != 0) {
+                psSelectIdParque = conn.prepareStatement(this.selectId);
+                psSelectIdParque.setString(1, this.textNombre.getText());
+                rs = psSelectIdParque.executeQuery();
+                rs.next();
+                id = rs.getInt("id");
+                JOptionPane.showMessageDialog(null, "Insercion correcta Se ha añadido el parque:" + id, "Ventana check", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Insercion incorrecta", "Ventana check", JOptionPane.WARNING_MESSAGE);
             }
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error" + ex.getMessage() +ex.getErrorCode(),"Ventana check",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "!!Error" + ex.getMessage() + ex.getErrorCode(), "Ventana check", JOptionPane.INFORMATION_MESSAGE);
         }
     }//GEN-LAST:event_bNuevoActionPerformed
 
     private void bEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEditarActionPerformed
+        PreparedStatement psUpdate = null;
+        String updateParque ;
         try {
-            ps=conn.prepareStatement(this.updateParque);
-            ps.setString(1,this.textNombre.getText());
-            ps.setDouble(2,Double.parseDouble(this.textExtension.getText()));
-            ps.setInt(3,Integer.parseInt(this.textCodigo.getText()));
-            ps.setInt(4,Integer.parseInt(this.textId.getText()));
-            int hecho=ps.executeUpdate();
-            if(hecho!=0){
-                JOptionPane.showMessageDialog(null,"Edicion correcta","Ventana check",JOptionPane.INFORMATION_MESSAGE);
-            }else{
-            JOptionPane.showMessageDialog(null,"Edicion incorrecta","Ventana check",JOptionPane.WARNING_MESSAGE);
+            if (!this.textNombre.getText().isEmpty() && comunidadModif != 0) {
+                updateParque = "update parque set  nombre = ?, idComunidad= ? where id = ?";
+                psUpdate = conn.prepareStatement(updateParque);
+                psUpdate.setString(1, this.textNombre.getText());
+                psUpdate.setInt(2, buscarId());
+                psUpdate.setInt(3, Integer.parseInt(this.textId.getText()));
+
+            }
+            if (!this.textExtension.getText().isEmpty() && comunidadModif != 0) {
+                updateParque = "update parque set  extension = ?, idComunidad = ? where id = ? ";
+                psUpdate = conn.prepareStatement(updateParque);
+                psUpdate.setDouble(1, Double.parseDouble(this.textExtension.getText()));
+                psUpdate.setInt(2, buscarId());
+                psUpdate.setInt(3, Integer.parseInt(this.textId.getText()));
+
+            }
+            if (!this.textNombre.getText().isEmpty()) {
+                updateParque = "update parque set nombre = ? where id = ?";
+                psUpdate = conn.prepareStatement(updateParque);
+                psUpdate.setString(1, this.textNombre.getText());
+                
+                psUpdate.setInt(2, Integer.parseInt(this.textId.getText()));               
+            }
+            if (!this.textExtension.getText().isEmpty()) {
+                updateParque = "update parque set extension = ? where id = ?";
+                psUpdate = conn.prepareStatement(updateParque);
+                psUpdate.setDouble(1, Double.parseDouble(this.textExtension.getText()));              
+                psUpdate.setInt(2, Integer.parseInt(this.textId.getText()));               
+            }
+            if(comunidadModif!=0){
+                updateParque=" update parque set idComunidad = ? where id = ?";
+                psUpdate = conn.prepareStatement(updateParque);
+                psUpdate.setInt(1, buscarId());
+                psUpdate.setInt(2, Integer.parseInt(this.textId.getText())); 
+            
+            }           
+            int hecho = psUpdate.executeUpdate();
+            if (hecho != 0) {
+                JOptionPane.showMessageDialog(null, "Edicion correcta", "Ventana check", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Edicion incorrecta", "Ventana check", JOptionPane.WARNING_MESSAGE);
             }
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error" + ex.getMessage() +ex.getErrorCode(),"Ventana check",JOptionPane.INFORMATION_MESSAGE);
- 
+            JOptionPane.showMessageDialog(null, "!!Error" + ex.getMessage() + ex.getErrorCode(), "Ventana check", JOptionPane.INFORMATION_MESSAGE);
+
         }
     }//GEN-LAST:event_bEditarActionPerformed
 
     private void bBorrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bBorrarActionPerformed
-        
+        PreparedStatement psDelete;
+        int hecho = 0;
+        try {
+            if (textNombre.getText().isEmpty() != true && this.textId.getText().isEmpty() == true) {
+                String delete1 = "delete from parque where nombre = ?";
+                psDelete = conn.prepareStatement(delete1);
+                psDelete.setString(1, (String) this.textNombre.getText());
+                hecho = psDelete.executeUpdate();
+            } else if (textId.getText().isEmpty() != true) {
+                String delete2 = "delete from parque where id = ?";
+                psDelete = conn.prepareStatement(delete2);
+                psDelete.setInt(1, Integer.parseInt(this.textId.getText()));
+                hecho = psDelete.executeUpdate();
+            }
+
+            if (hecho != 0) {
+                JOptionPane.showMessageDialog(null, " Borrado correcto", "Ventana check", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Borrado incorrecto", "Ventana check", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AppComunidades.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
     }//GEN-LAST:event_bBorrarActionPerformed
+
+    private void listaComunidadesItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_listaComunidadesItemStateChanged
+        comunidadModif = buscarId();
+    }//GEN-LAST:event_listaComunidadesItemStateChanged
 
     /**
      * @param args the command line arguments
@@ -278,19 +370,21 @@ public class AppComunidades extends javax.swing.JFrame {
     private javax.swing.JLabel lNombre;
     private javax.swing.JLabel lNombre1;
     private javax.swing.JLabel lParque;
-    private javax.swing.JTextField textCodigo;
+    private javax.swing.JComboBox listaComunidades;
     private javax.swing.JTextField textExtension;
     private javax.swing.JTextField textId;
     private javax.swing.JTextField textNombre;
     // End of variables declaration//GEN-END:variables
-    private String insertParque="insert into parque (nombre,extension,idComunidad) values(?,?,?) ";
-    private String updateParque = "update parque set nombre = ? , extension = ? , idComunidad= ? where id = ? ";
-    private String deleteParque= "delete from parque wheren ";
-    private PreparedStatement ps;
+    private String insertParque = "insert into parque (nombre,extension,idComunidad) values(?,?,?) ";
+
+    private String selectNombre = "select id from comunidad where nombre = ? ";
+    private String selectId = "select id from parque where nombre = ?";
     private Connection conn;
     private static String bd = "parques";
     private static String login = "root";
     private static String password = "root";
     private static String url = "jdbc:mysql://localhost:3306/" + bd;
-    
+    private int idComunidad = 0;
+    private int id = 0, comunidadModif = 0;
+    private ResultSet rs;
 }
